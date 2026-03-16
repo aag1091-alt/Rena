9f1baa5e31173ea21ac26ef3243cc293c314d615
Avinash Gosavi <aag1091@gmail.com>
2026-03-16 12:37:20 -0700

    bump SW cache to rena-v15

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rena-v14";
+const CACHE = "rena-v15";
 const STATIC = ["/", "/index.html", "/css/app.css", "/js/config.js", "/js/api.js", "/js/voice.js", "/js/app.js", "/worklet/audio-processor.js", "/manifest.json"];
 
 self.addEventListener("install", e => {
