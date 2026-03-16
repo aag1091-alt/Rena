bc30bd3fe8de5cee2ee75b3b2f16c3592ea7cf4e
Avinash Gosavi <aag1091@gmail.com>
2026-03-16 11:35:49 -0700

    Fix scroll on goal screen

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rena-v6";
+const CACHE = "rena-v7";
 const STATIC = ["/", "/index.html", "/css/app.css", "/js/config.js", "/js/api.js", "/js/voice.js", "/js/app.js", "/worklet/audio-processor.js", "/manifest.json"];
 
 self.addEventListener("install", e => {
